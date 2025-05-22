d16550185d7b1936ae2c706bef5647c4cb1db1a1
karlakryz-codes <karkakryz.codes@outlook.com>
2025-05-21 22:06:37 -0300

    Atualização

--- estrutura de dados/vetor.py
+++ estrutura de dados/vetor.py
@@ -7,11 +7,9 @@ for i in range(1, 11):  # Começa do 1 até 10
 
 print("\nLista preenchida com sucesso!\n")
 
-#for i in range(1, 11):  # Mesma lógica na hora de mostrar vai de 1 até 10
-    #print(f"O elemento da posição {i} é {lista[i - 1]}") # porque mesmo que você exiba como se começasse do 1, a lista internamente 
-    #começa no índice 0
-
-for i in range(1, 11,2):  # Mesma lógica na hora de mostrar vai de 1 até 10
+for i in range(1, 11):  # Mesma lógica na hora de mostrar vai de 1 até 10
     print(f"O elemento da posição {i} é {lista[i - 1]}") # porque mesmo que você exiba como se começasse do 1, a lista internamente 
     #começa no índice 0
+
+
     
